769231b9d1070754c1b086aa3dab41d1b3262008
Huỳnh Ngô Anh Thái <huynhngoanhthai@gmail.com>
2024-04-27 09:05:47 +0700

    add filter

--- index.js
+++ index.js
@@ -2,9 +2,15 @@ const Human = {
     name: "thai",
     age: 12
 };
-// log name and age
+const Dog = {
+    name: "nick",
+    age: 1
+};
+const listA = [Human,Dog];
+const a = listA.filter(i  => i.age > 2);
+console.log(a );
 
-if (Human) {
-    console.log(Human.name);
-    console.log(Human.name);
-}
+// if (Human) {
+//     console.log(Human.name);
+//     console.log(Human.name);
+// };
